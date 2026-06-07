0dc8039289c2a0a91cb8a9e5b4446eca734ffc7b
Morty-Pro <manamhastamsalam2@gmail.com>
2026-06-07 17:30:48 +0330

    try show battery percent, redo bitmap

--- ats-mini/Battery.cpp
+++ ats-mini/Battery.cpp
@@ -63,11 +63,6 @@ float batteryMonitor()
 }
 
 //
-// Edited by keyhan: new formula to calculate battery percent. after this, you need to change the drawing_battery_percent function or make a new display_func with Font.
-//
-// float batteryPercent = 100.0 * pow((voltage - 3.0) / 1.2, 1.5);
-
-//
 // Show last measured battery voltage and status at given screen
 // coordinates. Return true if voltage was drawn.
 //
@@ -120,6 +115,13 @@ bool drawBattery(int x, int y)
     // Text representation of the voltage
     sprintf(voltage, "%.02fV", batteryVolts);
 
+    //
+    // Edited by keyhan: new formula to calculate battery percent. after this, you need to change the drawing_battery_percent function or make a new display_func with Font.
+    //
+    char percent[8];
+    static float batteryPercent = 100.0 * pow((batteryVolts - 3.0) / 1.2, 1.5); // current battery percent
+    sprintf(percent, "%.02f%", batteryPercent);
+
     // Battery bar color and width
     switch(batteryState)
     {
@@ -143,7 +145,7 @@ bool drawBattery(int x, int y)
     }
 
     spr.fillRoundRect(x + 2, y + 3, level, 10, 2, color);
-    spr.drawString(voltage, x - 3, y, 2);
+    spr.drawString(percent, x - 3, y, 2);
     return true;
   }
 }
